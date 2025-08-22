d1d560d8030613031862e1ccc250c149745c0e26
Rajkumar Dedun <dedun.rajkumar.946@ldce.ac.in>
2025-08-22 09:26:33 +0530

    Added for cors configuration

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1 import auth, users, projects, bugs
 
 app = FastAPI(title="Bug Tracker API")
+
+# Allow Angular frontend
+origins = [
+    "http://localhost:4200",  # Angular dev server
+    # "https://your-production-domain.com",  # (later for production)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,        # or ["*"] for all, but not safe in prod
+    allow_credentials=True,
+    allow_methods=["*"],          # allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],          # allow all headers
+)
 
 app.include_router(auth.router, prefix="/api/auth", tags=["Auth"])
 app.include_router(users.router, prefix="/api/users", tags=["Users"])
